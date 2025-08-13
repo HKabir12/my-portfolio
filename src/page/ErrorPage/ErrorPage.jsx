ed8504bfd5462eb8c53cf560ff0d7da976f08c6d
Humaon Kabir <humaonkabir2003@gmail.com>
2025-08-13 20:06:56 +0600

    errorpage add

---
+++ src/page/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center text-center  p-4 font-sans text-white">
+      <h1 className="text-8xl md:text-9xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-pink-600 animate-pulse">
+        404
+      </h1>
+      <p className="mt-8 mb-10 text-xl md:text-2xl text-gray-300 max-w-md mx-auto leading-relaxed">
+        Oops! It looks like you've wandered off the path. The page you're
+        looking for was not found.
+      </p>
+      <a
+        href="/"
+        className="inline-block px-8 py-4 bg-gradient-to-r from-purple-600 to-indigo-700 text-white font-bold text-lg rounded-full shadow-lg hover:from-purple-700 hover:to-indigo-800 transition duration-300 transform hover:-translate-y-1 hover:scale-105"
+      >
+        Back to Home
+      </a>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,12 +2,17 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 
 import React from "react";
 import RootLayout from "../layouts/RootLayout";
+import ErrorPage from "../page/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
   },
+  {
+    path:"*",
+    Component: ErrorPage
+  }
 ]);
 
 export default router;
